92dd73f2517b8740e386f2b72fe7e559194d50ed
Kid <kidonng@gmail.com>
2020-10-30 18:32:43 +0800

    Add missing exports

--- mod.ts
+++ mod.ts
@@ -1,3 +1,7 @@
+export * from './config.ts'
+export * from './csv.ts'
 export { ky } from './deps.ts'
-export * from './graphql.ts'
 export * from './gist.ts'
+export * from './graphql.ts'
+export * from './json.ts'
+export * from './proxy.ts'
